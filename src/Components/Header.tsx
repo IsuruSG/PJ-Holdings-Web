d60d67ae49a5e0164f069c6550ed9eb12d3a4c34
MIHIRANGA99 <kavisha.mihiranga@yahoo.com>
2023-01-18 06:58:21 +0530

    Header Component: added logo and drawer on mobile UI

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,11 +1,22 @@
-import { AppBar, Box, IconButton, Toolbar } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  Button,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  Toolbar,
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import React, { useEffect, useState } from 'react';
 import NavItem from './NavItem';
 import IHeader from '@/Interfaces/IHeader';
+import Image from 'next/image';
 
-const Header:React.FC<IHeader> = ({itemList, invisible}) => {
+const Header: React.FC<IHeader> = ({ itemList, invisible }) => {
   const [currentLink, setCurrentLink] = useState<string>('');
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   useEffect(() => {
     setCurrentLink(window.location.pathname);
@@ -15,9 +26,41 @@ const Header:React.FC<IHeader> = ({itemList, invisible}) => {
     window.location.href = link;
   };
 
+  const drawerBox = () => (
+    <Box className="h-full">
+      <List className="flex flex-col h-full">
+        <section className="flex-auto h-2/6 flex justify-center items-center">
+          <Image
+            className="rounded-full"
+            alt="PJ - Holdings"
+            width={100}
+            height={100}
+            src="/logo.png"
+          />
+        </section>
+        <section className="flex-auto h-4/6 flex flex-col space-y-3">
+          {itemList.map(
+            (item: { name: string; link: string }, index: number) => (
+              <button
+                key={item.name + index}
+                className={`px-12 mx-2 rounded-lg py-2 text-lg ${
+                  currentLink === item.link
+                    ? 'bg-yellow-450 text-gray-850'
+                    : 'border-yellow-450 border'
+                }`}
+              >
+                {item.name}
+              </button>
+            )
+          )}
+        </section>
+      </List>
+    </Box>
+  );
+
   return (
-    <AppBar className={invisible? 'bg-transparent': ''}>
-      <Toolbar className='px-3 md:px-0 -my-1'>
+    <AppBar className={invisible ? 'bg-transparent' : 'overflow-hidden'}>
+      <Toolbar className="px-3 md:px-0 -my-1">
         <IconButton
           className="block md:hidden"
           size="large"
@@ -25,10 +68,18 @@ const Header:React.FC<IHeader> = ({itemList, invisible}) => {
           color="inherit"
           aria-label="menu"
           sx={{ mr: 2 }}
+          onClick={() => setIsOpen(!isOpen)}
         >
           <MenuIcon />
         </IconButton>
-        <Box className="-space-x-9 overflow-hidden hidden md:block">
+        <Box className="-space-x-9 hidden md:flex">
+          <Image
+            className="rounded-full p-2"
+            alt="PJ - Holdings"
+            width={60}
+            height={60}
+            src="/logo.png"
+          />
           {itemList.map(
             (item: { name: string; link: string }, index: number) => (
               <NavItem
@@ -40,6 +91,9 @@ const Header:React.FC<IHeader> = ({itemList, invisible}) => {
             )
           )}
         </Box>
+        <Drawer open={isOpen} onClose={() => setIsOpen(false)}>
+          {drawerBox()}
+        </Drawer>
       </Toolbar>
     </AppBar>
   );
