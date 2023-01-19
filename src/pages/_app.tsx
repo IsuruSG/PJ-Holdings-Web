2dca948836ad1fd9ce1b56c74c84f85c88697c8c
MIHIRANGA99 <kavisha.mihiranga@yahoo.com>
2023-01-19 08:35:06 +0530

    tailwindConfigs: added new two fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,30 @@
 import theme from '@/MUI/Themes';
 import '@/styles/globals.css';
 import { ThemeProvider } from '@mui/material';
-import { Inter } from '@next/font/google';
+import { Cookie, Inter, Righteous } from '@next/font/google';
 import type { AppProps } from 'next/app';
 
-const inter = Inter({ subsets: ['latin'], weight: '400' });
+const inter = Inter({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-inter',
+});
+const cookie = Cookie({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-cookie',
+});
+const righteous = Righteous({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-righteous',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={inter.className}>
+    <main
+      className={`${inter.variable} ${cookie.variable} ${righteous.variable} font-inter`}
+    >
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
