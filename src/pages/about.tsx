a98dd85513c843047f9b2a37dca3011cb9a4a052
MIHIRANGA99 <kavisha.mihiranga@yahoo.com>
2023-01-23 15:09:05 +0530

    About Us Page: fixed issies in the page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,27 +6,42 @@ import PageLayout from '@/Layouts/PageLayout';
 const About = () => {
   return (
     <PageLayout>
-      <div className="flex flex-col bg-gray-850 h-full mt-20 space-y-3">
-        {[''].map((data, index) => (
-          <div className="flex">
-            <svg
-              id="visual"
-              viewBox="0 0 900 600"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              version="1.1"
-            >
-              <path
-                d="M545 600L786 0L900 0L900 600Z"
-                fill="#F3A904"
-                stroke-linecap="square"
-                stroke-linejoin="bevel"
-              ></path>
-            </svg>
-            {/* <div className="bg-yellow-450 w-2/3">huiohui</div> */}
-            <section>
+      <div className="flex flex-col min-h-screen bg-gray-850 pt-20 space-y-10">
+        {aboutList.map((data, index) => (
+          <div className="flex h-56 ">
+            <section className={index % 2 === 0 ? 'hidden' : 'block -mr-1'}>
+              <svg
+                className="rotate-180"
+                id="visual"
+                height="100%"
+                width="100%"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                version="1.1"
+              >
+                <path
+                  d="M75 0L411 600L0 600L0 0Z"
+                  fill="#F3A904"
+                  stroke-linecap="square"
+                  stroke-linejoin="bevel"
+                ></path>
+              </svg>
+            </section>
+            <section className="bg-yellow-450 w-full flex flex-col justify-between py-12 px-4 items-center">
+              <div className="text-center text-2xl font-black">
+                {data.title}
+              </div>
+              <div
+                className={`${index % 2 === 1 ? 'text-right' : 'text-left'}`}
+              >
+                {data.description}
+              </div>
+            </section>
+            <section className={index % 2 === 1 ? 'hidden' : 'block'}>
               <svg
                 id="visual"
+                height="100%"
+                width="100%"
                 xmlns="http://www.w3.org/2000/svg"
                 xmlnsXlink="http://www.w3.org/1999/xlink"
                 version="1.1"
