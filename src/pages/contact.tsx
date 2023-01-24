6dd8af0221f015516580eecbf1eac2e868aa5741
Isuru <mgisudeepa99@gmail.com>
2023-01-24 17:13:36 +0530

    Contact Us page:changed the description

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -5,19 +5,20 @@ import React from 'react';
 const contact = () => {
   return (
     <PageLayout>
-      <section className="flex flex-col justify-center items-center min-h-screen py-20">
-        <span className="text-3xl mb-9 text-center font-black text-white lg:hidden">
+      <section className="flex flex-col items-center justify-center min-h-screen py-20">
+        <span className="text-3xl font-black text-center text-white mb-9 lg:hidden">
           Contact Us
         </span>
         <div className="w-3/5">
           <ContactCard>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            rhoncus, turpis eu egestas cursus, nisi velit semper neque, nec
-            vehicula nunc arcu in ligula. Aliquam quis enim eu lacus sodales
-            vulputate. Duis ut fermentum lectus, ut lobortis elit. Etiam egestas
-            velit id nisi euismod tristique. Nullam quis mauris ac erat rutrum
-            sagittis. Etiam egestas velit id nisi euismod tristique. Nullam quis
-            mauris ac erat rutrum sagittis.
+            At SIA PJ Holdings, we are committed to providing our clients with
+            the highest level of service and support. Our dedicated team of
+            professionals is always available to answer any questions you may
+            have about our company or our services. Whether you're a current
+            client or just looking for more information, we're here to help.
+            Please don't hesitate to reach out to us via phone, email or by
+            filling out the contact form on our website. We look forward to
+            hearing from you and working with you on your next building project.
           </ContactCard>
         </div>
       </section>
